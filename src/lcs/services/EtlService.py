4472ab074a5163f4bba5306eafcff9d9d38f0c5d
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-11-18 12:05:37 -0500

    add iqrChekOutlierValues

--- src/lcs/services/EtlService.py
+++ src/lcs/services/EtlService.py
@@ -1,9 +1,9 @@
 from services.SingletonMeta import SingletonMeta
 import pandas as pd
 import numpy as np
+import jenkspy
 import joblib
 from sklearn import linear_model
-from sympy.ntheory import primefactors as pf
 import os
 
 class EtlService(metaclass=SingletonMeta):
@@ -30,7 +30,7 @@ class EtlService(metaclass=SingletonMeta):
         # Same format
         data = self.sameFormat(data)
         # Checking outlier values
-        #data = self.IQR_ChekOutlierValues(data)
+        data = self.iqrChekOutlierValues(data)
         # Featurization data
         #data = self.featurizationData(data)
         return data
@@ -63,6 +63,66 @@ class EtlService(metaclass=SingletonMeta):
         filterComprobation = ((data == 'OTHER') | (data == 'CURL') | (data == 'NONE')).any()
         print(len(data.loc[:, filterComprobation].columns.tolist()))
         return data
+
+    def iqrChekOutlierValues(self, dataDeposits):
+        # checking outlier values
+        q1_amount_transaction_amount = dataDeposits['transaction amount'].quantile(.25)
+        q3_amount_transaction_amount = dataDeposits['transaction amount'].quantile(.75)
+        IQR_amount = q3_amount_transaction_amount - q1_amount_transaction_amount
+        print('transaction amount IQR: ', IQR_amount)
+
+        # defining limits
+        sup_amount_transaction_amount = q3_amount_transaction_amount + 1.5 * IQR_amount
+        inf_amount_transaction_amount = q1_amount_transaction_amount - 1.5 * IQR_amount
+        print('transaction amount Upper limit: ', sup_amount_transaction_amount)
+        print('transaction amount Lower limit: ', inf_amount_transaction_amount)
+
+        # checking outlier values
+        q1_amount_user_balance = dataDeposits['user balance'].quantile(.25)
+        q3_amount_user_balance = dataDeposits['user balance'].quantile(.75)
+        IQR_amount = q3_amount_user_balance - q1_amount_user_balance
+        print('user balance IQR: ', IQR_amount)
+        # defining limits
+        sup_amount_user_balance = q3_amount_user_balance + 1.5 * IQR_amount
+        inf_amount_user_balance = q1_amount_user_balance - 1.5 * IQR_amount
+        print('user balance Upper limit: ', sup_amount_user_balance)
+        print('user balance Lower limit: ', inf_amount_user_balance)
+
+        # cleaning the outliers in 'transaction amount' values
+        dataDeposits_clean_transaction = dataDeposits.copy()
+        dataDeposits_clean_transaction.drop(
+            dataDeposits_clean_transaction[dataDeposits_clean_transaction['transaction amount'] >
+                                        sup_amount_transaction_amount].index, axis=0, inplace=True)
+        dataDeposits_clean_transaction.drop(
+            dataDeposits_clean_transaction[dataDeposits_clean_transaction['transaction amount'] <
+                                        inf_amount_transaction_amount].index, axis=0, inplace=True)
+
+        # cleaning the outliers in 'user balance' values
+        dataDeposits_clean_balance = dataDeposits.copy()
+        dataDeposits_clean_balance.drop(dataDeposits_clean_balance[dataDeposits_clean_balance['user balance'] >
+                                                                sup_amount_user_balance].index, axis=0, inplace=True)
+        dataDeposits_clean_balance.drop(dataDeposits_clean_balance[dataDeposits_clean_balance['user balance'] <
+                                                                inf_amount_user_balance].index, axis=0, inplace=True)
+
+        # create nominal intervals 
+        dataDeposits = self.jenksBreakMethod('transaction amount', dataDeposits_clean_transaction, dataDeposits)
+        dataDeposits = self.jenksBreakMethod('user balance', dataDeposits_clean_balance, dataDeposits)
+
+        return dataDeposits
+
+    def jenksBreakMethod(self, name_feature, dataDepositByFeature, dataDeposits):
+        # with cleaning outliers 'transaction amount'
+        labels = ['small', 'medium', 'big', 'outlier']
+        breaks = jenkspy.jenks_breaks(dataDepositByFeature[name_feature], nb_class=3)
+        breaks.append(dataDeposits[name_feature].max()) # tener en cuenta el el rango minimo
+        numb_Bins = len(breaks) - 1
+
+        print(breaks)
+        print(numb_Bins)
+
+        dataDeposits[name_feature] = pd.cut(dataDeposits[name_feature], bins=breaks, labels=labels, include_lowest=True)
+        return dataDeposits
+
 
     def save_object(self, filename, model):
         with open('' + filename, 'wb') as file:
